68fca1a06ccd5909b0625dcc95c7197b8b04b4ee
Yikai Shi <yikaishi@Yikais-MacBook-Pro.local>
2019-01-16 01:43:36 +0800

    added functionality to rip meta data from Line website

--- src/routes/addWord.js
+++ src/routes/addWord.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import moment from 'moment';
+import axios from 'axios';
+import config from '../../config';
 import { chineseCollection } from '../utils/mongo';
+import { parseDictionaryResults } from '../utils/utils';
 
 const router = express.Router();
 
@@ -43,6 +46,66 @@ router.post('/', async (req, res, next) => {
       code: 'add.word.success',
       message: `IDs ${updatedData.map(({ id }) => id).join(', ')}, and words ${updatedData.map(({ character }) => character).join(', ')} added successfully`,
     });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+router.post('/search', async (req, res, next) => {
+  try {
+    const { id, character, notes } = req.body;
+
+    // Decide what ID to use (latest from mongo+1 or what user inputs)
+
+    const encodedChar = encodeURI(character);
+    const { dictionaryHost } = config;
+    const querySt = '010';
+    const queryRlt = '000';
+    const queryEnc = 'UTF-8';
+    const queryRFormat = 'json';
+    const queryREnc = 'UTF-8';
+
+    const queryUrl = `${dictionaryHost}?q=${encodedChar}&st=${querySt}&r_lt=${queryRlt}&q_enc=${queryEnc}&r_format=${queryRFormat}&r_enc=${queryREnc}`;
+
+    try {
+      const response = await axios.get(queryUrl);
+      const results = response.data.items[1];
+
+      const parsedData = parseDictionaryResults(results);
+
+      const dateAdded = moment().format();
+
+      const updatedData = {
+        ...parsedData,
+        id,
+        notes,
+        dateAdded,
+        dateModified: dateAdded,
+        timesTested: 0,
+        timesCorrect: 0,
+        timesIncorrect: 0,
+        useForReview: true,
+      };
+
+      try {
+        await chineseCollection().insertOne(updatedData);
+      } catch ({ message }) {
+        return res.status(500).json({
+          code: 'add.word.mongo.fail',
+          message,
+        });
+      }
+
+      return res.send({
+        code: 'add.word.search.success',
+        message: `ID: ${id} | Word: ${character} successfully added`,
+      });
+    } catch ({ message }) {
+      return res.status(500).json({
+        code: 'search.dictionary.fail',
+        message,
+      });
+    }
   } catch (e) {
     return next(e);
   }
